16f5aca2a56c1eafbe14f1bdad57baf2322198ad
skaimarvas <skaimarvas@gmail.com>
2023-11-22 20:46:22 +0300

    DELETE_FAVORITE actionu eklendi

--- src/store/actions/favoriteActions.js
+++ src/store/actions/favoriteActions.js
@@ -1,5 +1,9 @@
 export const ADD_FAVORITE = "ADD_FAVORITE";
+export const DELETE_FAVORITE = "DELETE_FAVORITE";
 
+export const deleteFavorite = (id) => {
+  return { type: DELETE_FAVORITE, payload: id };
+};
 export const addFavoriteMovie = (movie) => {
   return { type: ADD_FAVORITE, payload: movie };
 };
--- src/store/reducers/favoriteReducer.js
+++ src/store/reducers/favoriteReducer.js
@@ -1,5 +1,5 @@
 import { movies } from "../../movies";
-import { ADD_FAVORITE } from "../actions/favoriteActions";
+import { ADD_FAVORITE, DELETE_FAVORITE } from "../actions/favoriteActions";
 
 const initialState = {
   favMovies: [movies[0]],
@@ -9,6 +9,11 @@ export const favoriteReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_FAVORITE:
       return { ...state, favMovies: [...state.favMovies, action.payload] };
+    case DELETE_FAVORITE:
+      return {
+        ...state,
+        favMovies: state.favMovies.filter((fav) => fav.id !== action.payload),
+      };
     default:
       return state;
   }
